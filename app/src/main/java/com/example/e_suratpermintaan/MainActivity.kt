9f4a020677f87968d8ef92def68746d857ba28d0
Yoga Darma <kadekyoga125@gmail.com>
2020-04-07 17:53:02 +0800

    first commit

---
+++ app/src/main/java/com/example/e_suratpermintaan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_suratpermintaan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
