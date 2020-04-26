fa69f46e526f44eeab6227d4c37fda05a1fd99ea
Yoga Darma <kadekyoga125@gmail.com>
2020-04-26 10:38:27 +0800

    adding function click from notifikasi activity to detail activity

--- app/src/main/java/com/example/e_suratpermintaan/presentation/activity/NotifikasiActivity.kt
+++ app/src/main/java/com/example/e_suratpermintaan/presentation/activity/NotifikasiActivity.kt
@@ -1,9 +1,15 @@
 package com.example.e_suratpermintaan.presentation.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.e_suratpermintaan.core.domain.entities.requests.ReadNotifikasi
 import com.e_suratpermintaan.core.domain.entities.responses.DataNotifikasi
 import com.e_suratpermintaan.core.domain.entities.responses.NotifikasiResponse
+import com.e_suratpermintaan.core.domain.entities.responses.ReadItem
+import com.e_suratpermintaan.core.domain.entities.responses.UnreadItem
 import com.example.e_suratpermintaan.R
 import com.example.e_suratpermintaan.framework.sharedpreference.ProfilePreference
 import com.example.e_suratpermintaan.presentation.adapter.NotifikasiAdapter
@@ -18,16 +24,36 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class NotifikasiActivity : BaseActivity() {
 
     private val notifikasiViewModel: NotifikasiViewModel by viewModel()
+    private val readNotifikasiViewModel: NotifikasiViewModel by viewModel()
     private val profilePreference: ProfilePreference by inject()
+
+    private lateinit var notifikasiAdapter: NotifikasiAdapter
+    private var idUser: String? = null
 
     override fun layoutId(): Int = R.layout.activity_notifikasi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val idUser = profilePreference.getProfile()?.id
+        init()
 
+    }
+
+    private fun init(){
+        idUser = profilePreference.getProfile()?.id
         getNotifikasi(idUser)
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        (recyclerView.layoutManager as LinearLayoutManager).isAutoMeasureEnabled = true
+        recyclerView.setHasFixedSize(false)
+
+    }
+
+    private fun setupListener() {
 
     }
 
@@ -59,10 +85,27 @@ class NotifikasiActivity : BaseActivity() {
             }
         }
 
-        val notifikasiAdapter = NotifikasiAdapter(dataAllNotif, viewType)
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        notifikasiAdapter = NotifikasiAdapter(dataAllNotif, viewType)
+        notifikasiAdapter.notifyDataSetChanged()
         recyclerView.adapter = notifikasiAdapter
+
+        notifikasiAdapter.setOnClickListener(object : NotifikasiAdapter.OnClickItemListener{
+            override fun onClick(view: View, item: Any) {
+                val dataClick = item
+
+                val intent = Intent(this@NotifikasiActivity, DetailSuratPermintaanActivity::class.java)
+                if (dataClick is UnreadItem){
+                    toastNotify("Unread")
+                    disposable = readNotifikasiViewModel.readNotifikasi(ReadNotifikasi(idUser.toString(), dataClick.id.toString()))
+                        .subscribe()
+                    intent.putExtra("id_sp", dataClick.id_sp)
+                } else if (dataClick is ReadItem){
+                    toastNotify("Read")
+                    intent.putExtra("id_sp", dataClick.id_sp)
+                }
+                startActivity(intent)
+            }
+        })
     }
 
     private fun handleError(error: Throwable) {
